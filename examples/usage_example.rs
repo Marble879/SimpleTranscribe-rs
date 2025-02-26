5180b4d7607984f6ddaf55ffeaf99cd986f004cc
WENPIN1 <WENPIN1@users.noreply.github.com>
2025-02-26 10:31:33 +0800

    Update usage_example.rs
    Update usage_example.rs for Chinese voice test
    16KHz sampling rate .wav format is better.

--- examples/usage_example.rs
+++ examples/usage_example.rs
@@ -3,9 +3,11 @@ use simple_transcribe_rs::transcriber;
 
 #[tokio::main]
 async fn main() {
-    let m = model_handler::ModelHandler::new("tiny", "models/").await;
+    let m = model_handler::ModelHandler::new("large", "models/").await;
     let trans = transcriber::Transcriber::new(m);
-    let result = trans.transcribe("src/test_data/test.mp3", None).unwrap();
+    let mut params = FullParams::new(SamplingStrategy::default());
+    params.set_language(Some("zh")); // 16KHz sampling rate .wav format is better.
+    let result = trans.transcribe("src/test_data/GongXiFaChai.wav",  Some(params)).unwrap();
     let text = result.get_text();
     let start = result.get_start_timestamp();
     let end = result.get_end_timestamp();
