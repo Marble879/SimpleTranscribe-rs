294ab7a0a7e153aae8086b54d2705d1abd69a655
WENPIN1 <WENPIN1@users.noreply.github.com>
2025-03-06 14:41:37 +0800

    Create usage_example_chinese.rs
    Create usage_example_chinese.rs for Chinese voice.

---
+++ examples/usage_example_chinese.rs
@@ -0,0 +1,17 @@
+use simple_transcribe_rs::model_handler;
+use simple_transcribe_rs::transcriber;
+use whisper_rs::FullParams;
+use whisper_rs::SamplingStrategy;
+
+#[tokio::main]
+async fn main() {
+    let m = model_handler::ModelHandler::new("large", "models/").await;
+    let trans = transcriber::Transcriber::new(m);
+    let mut params = FullParams::new(SamplingStrategy::default());
+    params.set_language(Some("zh")); // 16KHz sampling rate .wav format is better.
+    let result = trans.transcribe("src/test_data/GongXiFaChai.wav",  Some(params)).unwrap();
+    let text = result.get_text();
+    let start = result.get_start_timestamp();
+    let end = result.get_end_timestamp();
+    println!("start[{}]-end[{}] {}", start, end, text);
+}
